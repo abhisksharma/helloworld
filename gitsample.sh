31d4fd218ac4d39835eec590acd1760ad550d39e
Abhishek Sharma <sharma.abhi.jal@gmail.com>
2018-03-05 00:46:17 +0530

    Brach add

--- gitsample.sh
+++ gitsample.sh
@@ -3,8 +3,5 @@
 echo "Hello world"
 echo " Hello this si my second line in git"
 echo " This is changes from git web "
-<<<<<<< HEAD
 echo " Added after pull"
-=======
 echo " This is after pull"
->>>>>>> 155ee54113c10abda7e37dce8ebe0ff4ef2c6bcd
